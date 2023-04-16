ad9b3da06a82d0f7bd4203d15f248a81ae85b116
ChenemiKen <chenemiken15@gmail.com>
2023-04-16 22:58:01 +0100

    service-registry

---
+++ servcie-registry/src/test/java/com/chenemiken/servcieregistry/ServcieRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenemiken.servcieregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServcieRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
